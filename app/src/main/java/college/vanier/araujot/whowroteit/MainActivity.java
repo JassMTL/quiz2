d6a2d0429d6076e632fbbd4906fe206f2700d0c8
cstuser <u>
2019-10-22 13:33:24 -0400

    ukk

---
+++ app/src/main/java/college/vanier/araujot/whowroteit/MainActivity.java
@@ -0,0 +1,236 @@
+package college.vanier.araujot.whowroteit;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.app.LoaderManager;
+import androidx.loader.content.Loader;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<String> {
+
+    private EditText mBookInput;
+    private TextView mTitleText;
+    private TextView mAuthorText;
+    private ImageView mImageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mBookInput = findViewById(R.id.bookInput);
+        mTitleText = findViewById(R.id.titleText);
+        mAuthorText = findViewById(R.id.authorText);
+        mImageView = findViewById(R.id.imageView);
+    }
+
+    public void searchBooks(View view) {
+        String queryString = mBookInput.getText().toString();
+        InputMethodManager inputManager = (InputMethodManager)
+                getSystemService(Context.INPUT_METHOD_SERVICE);
+
+        if (inputManager != null ) {
+            inputManager.hideSoftInputFromWindow(view.getWindowToken(),
+                    InputMethodManager.HIDE_NOT_ALWAYS);
+        }
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = null;
+        if (connMgr != null) {
+            networkInfo = connMgr.getActiveNetworkInfo();
+        }
+        if (networkInfo != null && networkInfo.isConnected()
+                && queryString.length() != 0) {
+            if (view.getId()==R.id.searchButton) {
+                //new FetchBook(mTitleText, mAuthorText).execute(queryString);
+                String LOG_TAG =
+                        NetworkUtils.class.getSimpleName();
+                // Base URL for Books API.
+                String BOOK_BASE_URL =  "https://www.googleapis.com/books/v1/volumes?";
+                // Parameter for the search string.
+                String QUERY_PARAM = "q";
+                // Parameter that limits search results.
+                String MAX_RESULTS = "maxResults";
+                // Parameter to filter by print type.
+                String PRINT_TYPE = "printType";
+                Uri builtURI = Uri.parse(BOOK_BASE_URL).buildUpon()
+                        .appendQueryParameter(QUERY_PARAM, queryString)
+                        .appendQueryParameter(MAX_RESULTS, "10")
+                        .appendQueryParameter(PRINT_TYPE, "books")
+                        .build();
+                String url = builtURI.toString();
+                RequestQueue queue = Volley.newRequestQueue(this);
+                StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                try {
+                                    JSONObject jsonObject = new JSONObject(response);
+                                    JSONArray itemsArray = jsonObject.getJSONArray("items");
+                                    int i = 0;
+                                    String title = null;
+                                    String authors = null;
+                                    String thumbnailImg;
+
+
+                                    while (i < itemsArray.length() &&
+                                            (authors == null && title == null)) {
+                                        // Get the current item information.
+                                        JSONObject book = itemsArray.getJSONObject(i);
+                                        JSONObject volumeInfo = book.getJSONObject("volumeInfo");
+                                        JSONObject imageLinks = volumeInfo.getJSONObject("imageLinks");
+                                        // Try to get the author and title from the current item,
+                                        // catch if either field is empty and move on.
+                                        try {
+                                            title = volumeInfo.getString("title");
+                                            Log.d("Image",title);
+                                            authors = volumeInfo.getString("authors");
+                                            Log.d("Image",authors);
+
+                                            thumbnailImg = imageLinks.getString("thumbnail");
+                                            Log.d("Image",thumbnailImg);
+
+                                            if (title != null && authors != null) {
+                                                mTitleText.setText(title);
+                                                mAuthorText.setText(authors);
+                                                Picasso.get().setLoggingEnabled(true);
+                                                Picasso.get().load(thumbnailImg).into(mImageView);
+
+                                            } else {
+                                                mTitleText.setText(R.string.no_results);
+                                                mAuthorText.setText("");
+                                            }
+                                        } catch (Exception e) {
+                                            e.printStackTrace();
+                                        }
+
+                                        // Move to the next item.
+                                        i++;
+                                    }
+                                } catch (Exception e) {
+                                    // If onPostExecute does not receive a proper JSON string,
+                                    // update the UI to show failed results.
+                                    mTitleText.setText(R.string.no_results);
+                                    mAuthorText.setText("");
+                                }
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {}
+                });
+                queue.add(stringRequest);
+
+
+
+
+
+            } else {
+                Bundle queryBundle = new Bundle();
+                queryBundle.putString("queryString", queryString);
+                LoaderManager.getInstance(this).restartLoader(0, queryBundle, this);
+            }
+            mAuthorText.setText("");
+            mTitleText.setText(R.string.loading);
+        } else {
+            if (queryString.length() == 0) {
+                mAuthorText.setText("");
+                mTitleText.setText(R.string.no_search_term);
+            } else {
+                mAuthorText.setText("");
+                mTitleText.setText(R.string.no_network);
+            }
+        }
+    }
+
+    @NonNull
+    @Override
+    public Loader<String> onCreateLoader(int id, @Nullable Bundle args) {
+        String queryString = "";
+
+        if (args != null) {
+            queryString = args.getString("queryString");
+        }
+
+        return new BookLoader(this, queryString);
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<String> loader, String data) {
+        try {
+            JSONObject jsonObject = new JSONObject(data);
+            JSONArray itemsArray = jsonObject.getJSONArray("items");
+            int i = 0;
+            String title = null;
+            String authors = null;
+            String thumbnailImg;
+
+
+            while (i < itemsArray.length() &&
+                    (authors == null && title == null)) {
+                // Get the current item information.
+                JSONObject book = itemsArray.getJSONObject(i);
+                JSONObject volumeInfo = book.getJSONObject("volumeInfo");
+                JSONObject imageLinks = volumeInfo.getJSONObject("imageLinks");
+                // Try to get the author and title from the current item,
+                // catch if either field is empty and move on.
+                try {
+                    title = volumeInfo.getString("title");
+                    Log.d("Image",title);
+                    authors = volumeInfo.getString("authors");
+                    Log.d("Image",authors);
+
+                    thumbnailImg = imageLinks.getString("thumbnail");
+                    Log.d("Image",thumbnailImg);
+
+                    if (title != null && authors != null) {
+                        mTitleText.setText(title);
+                        mAuthorText.setText(authors);
+                        Picasso.get().setLoggingEnabled(true);
+                        Picasso.get().load(thumbnailImg).into(mImageView);
+
+                    } else {
+                        mTitleText.setText(R.string.no_results);
+                        mAuthorText.setText("");
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                // Move to the next item.
+                i++;
+            }
+        } catch (Exception e) {
+            // If onPostExecute does not receive a proper JSON string,
+            // update the UI to show failed results.
+            mTitleText.setText(R.string.no_results);
+            mAuthorText.setText("");
+        }
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<String> loader) {
+    }
+}
